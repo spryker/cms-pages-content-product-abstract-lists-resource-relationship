e0fdcac494551b30344ddad0cadb84e644339ea0
Kotiuk <andrii.kotiuk@spryker.com>
2020-06-22 17:54:43 +0300

    mp-2855 created CmsPagesContentProductAbstractListsResourceRelationship module

---
+++ src/Spryker/Glue/CmsPagesContentProductAbstractListsResourceRelationship/Dependency/RestApiResource/CmsPagesContentProductAbstractListsResourceRelationshipToContentProductAbstractListsRestApiResourceInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Glue\CmsPagesContentProductAbstractListsResourceRelationship\Dependency\RestApiResource;
+
+use Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface;
+
+interface CmsPagesContentProductAbstractListsResourceRelationshipToContentProductAbstractListsRestApiResourceInterface
+{
+    /**
+     * @phpstan-param array<string, string> $contentProductAbstractListKeys
+     *
+     * @phpstan-return array<string, \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface>
+     *
+     * @param string[] $contentProductAbstractListKeys
+     * @param \Spryker\Glue\GlueApplication\Rest\Request\Data\RestRequestInterface $restRequest
+     * @param string $storeName
+     *
+     * @return \Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface[]
+     */
+    public function getContentProductAbstractListsByKeys(array $contentProductAbstractListKeys, RestRequestInterface $restRequest, string $storeName): array;
+}
